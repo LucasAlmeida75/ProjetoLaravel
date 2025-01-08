b6ae65a9f427cc4ae992210fe581c274eac00e01
LucasAlmeida75 <lucasalmeidamachado1@gmail.com>
2025-01-08 11:04:17 -0300

    feat: upload de imagens

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,10 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+            @php
+                $image = $event->image ? "/img/events/$event->image" : "/img/event_placeholder.jpg";
+            @endphp
+            <img src="{{ $image }}" alt="{{ $event->title }}">
             <div class="card-body">
                 <p class="card-date">10/09/2020</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
